2f76a07617e5dfc944a736a257e06309dee4845d
Frankie Baffa <frankiebaffa@gmail.com>
2021-09-30 18:05:06 -0400

    use WormDb trait

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,37 +1,7 @@
-use crate::context::Context;
-use rusqlite::Connection;
 use worm_derive::WormDb;
 use worm::DbContext;
 #[derive(WormDb)]
 pub struct Database {
     pub context: DbContext,
-}
-pub struct Db {}
-impl Db {
-    pub fn attach_temp_db<'db>(connection: &'db mut Connection, context: &Context) -> Result<&'db mut Connection, rusqlite::Error> {
-        let db_name = context.get_db_name();
-        connection.execute(
-            &format!(
-                "attach ':memory:' as {}",
-                db_name
-            ),
-            []
-        )?;
-        return Ok(connection);
-    }
-    pub fn attach_db<'db>(connection: &'db mut Connection, context: &Context) -> Result<&'db mut Connection, rusqlite::Error> {
-        let db_path = context.get_db_path();
-        let db_name = context.get_db_name();
-        connection.execute(
-            &format!(
-                "attach '{}/{}.db' as {}",
-                db_path,
-                db_name,
-                db_name
-            ),
-            []
-        )?;
-        return Ok(connection);
-    }
 }
 
