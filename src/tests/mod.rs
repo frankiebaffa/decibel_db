ee206e197321d025b036da34828746c8292defff
Frankie B <frankiebaffa@gmail.com>
2022-05-25 15:23:27 -0400

    use uuid for test db creation+flesh out tests

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,58 +1,69 @@
 use {
-    crate::migrator::DecibelMigrator,
+    crate::{
+        artist::Artist,
+        migrator::DecibelMigrator,
+    },
     sqlx::SqlitePool,
-    std::path::PathBuf,
+    std::{
+        future::Future,
+        path::PathBuf,
+    },
+    uuid::Uuid,
 };
 fn get_file_name() -> String {
-    let mut num = 0;
-    let mut name;
-    loop {
-        name = format!(
-            "./test_dbs/rusters_{}.db",
-            num
-        );
-        let chk = PathBuf::from(&name);
-        if !chk.exists() {
-            break;
-        }
-        num = num + 1;
-    }
+    let uuid = Uuid::new_v4();
+    let name = format!(
+        "./test_dbs/rusters_{}.db",
+        uuid.as_hyphenated()
+    );
     name
 }
-fn create_db_file_if_not_exist<'a>(name: &'a str) {
+fn create_db_file<'a>(name: &'a str) {
     let db_path = PathBuf::from(name);
-    if !db_path.exists() {
-        let file_res = std::fs::File::create(db_path);
-        file_res.unwrap();
-    }
+    std::fs::File::create(db_path)
+        .unwrap();
 }
-fn delete_db_file_if_exists<'a>(name: &'a str) {
+fn delete_db_file<'a>(name: &'a str) {
     let db_path = PathBuf::from(name);
-    if db_path.exists() {
-        let rem_res = std::fs::remove_file(db_path);
-        rem_res.unwrap();
-    }
+    std::fs::remove_file(db_path)
+        .unwrap();
     let db_wal = PathBuf::from(format!("{}-wal", name));
-    if db_wal.exists() {
-        let rem_res = std::fs::remove_file(db_wal);
-        rem_res.unwrap();
-    }
+    std::fs::remove_file(db_wal)
+        .unwrap();
     let db_shm = PathBuf::from(format!("{}-shm", name));
-    if db_shm.exists() {
-        let rem_res = std::fs::remove_file(db_shm);
-        rem_res.unwrap();
-    }
+    std::fs::remove_file(db_shm)
+        .unwrap();
 }
 async fn get_db<'a>(name: &'a str) -> SqlitePool {
     let path = format!("sqlite://{}", name);
     SqlitePool::connect(&path).await.unwrap()
 }
+async fn db_test<T, Fut>(function: T)
+where
+    T: Fn(SqlitePool) -> Fut,
+    Fut: Future,
+{
+    let db_name = get_file_name();
+    create_db_file(&db_name);
+    let db = get_db(&db_name).await;
+    function(db).await;
+    delete_db_file(&db_name);
+}
+async fn do_migrate(db: SqlitePool) {
+    DecibelMigrator::migrate(&db).await.unwrap();
+}
 #[async_std::test]
 async fn migrate_up() {
-    let db_name = get_file_name();
-    create_db_file_if_not_exist(&db_name);
-    let db = get_db(&db_name).await;
-    DecibelMigrator::migrate(&db).await.unwrap();
-    delete_db_file_if_exists(&db_name);
+    db_test(do_migrate).await;
 }
-
+async fn do_insert_artist(db: SqlitePool) {
+    DecibelMigrator::migrate(&db).await.unwrap();
+    let artist = Artist::insert(&db, "Test Artist".to_string())
+        .await
+        .unwrap();
+    assert_eq!(artist.get_name(), "Test Artist".to_string());
+}
+#[async_std::test]
+async fn ins_artist() {
+    db_test(do_insert_artist).await;
+}
